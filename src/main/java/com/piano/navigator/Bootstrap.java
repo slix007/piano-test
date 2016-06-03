55324c108e39d0334fcf241fa6a1666b7032601f
Sergey Shurmin <sergey.shurmin@paxport.se>
2016-06-03 11:19:34 +0200

    fix heroku deploy3

--- src/main/java/com/piano/navigator/Bootstrap.java
+++ src/main/java/com/piano/navigator/Bootstrap.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import spark.template.mustache.MustacheTemplateEngine;
 
 import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.setPort;
 import static spark.Spark.staticFiles;
 
 
@@ -21,17 +21,30 @@ public class Bootstrap {
     public static void main(String[] args) {
         staticFiles.location("/public");
 
-        port(getHerokuAssignedPort());
+        setHerokuAssignedPort();
+//        port(getHerokuAssignedPort());
         get("/", new HomeController().index, new MustacheTemplateEngine());
 
         get("/search-result", new SearchController().index, new MustacheTemplateEngine());
     }
 
-    private static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//    private static int getHerokuAssignedPort() {
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        if (processBuilder.environment().get("PORT") != null) {
+//            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//        }
+//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+//    }
+
+    private static void setHerokuAssignedPort() {
+        // Get port config of heroku on environment variable
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
         }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        setPort(port);
     }
 }
